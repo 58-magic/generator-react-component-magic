bc6a70e3042e31ae8aa8b1467737e1c0b1772fb7
damujiangr <damujiangr@aliyun.com>
2018-03-09 10:37:10 +0800

    download template and merge `package.json`

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# generator-react-component-magic
+# generator-react-component-magic【BETA】
 generator for react component of 58-magic
+
+> 默认template：[react-component-template](https://github.com/58-magic/react-component-template)
+
+#TODO
+- 处理package.json
+- 替换字符
+- 变量配置化
+- 函数封装
+- loading进度显示
+- 替换测试coveralls为codecov
+- 解决回调地狱，重构调用流程
--- generators/app/index.js
+++ generators/app/index.js
@@ -1,40 +1,78 @@
-'use strict';
 const Generator = require('yeoman-generator');
 const chalk = require('chalk');
 const yosay = require('yosay');
 
+const initJson = require('init-package-json');
+const DownloadGit = require('download-git-repo');
+
+const path = require('path');
+const fsextra = require('fs-extra');
+
+const archiveDir = 'temp';
+// const defaultComponentName = 'react-component-template';
+
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
+  initializing() {
     this.log(
       yosay(`
       Welcome to the phenomenal ${chalk.red('generator-react-component-magic')} generator!
       `)
     );
+    this._download();// eslint-disable-line
+  }
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
+  _writeJson() {
+    this.log('merge package.json');
+    const initFile = path.resolve(process.env.HOME, '.npm-init');
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
+    // the dir where we're doin stuff.
+    const dir = process.cwd();
+
+    const configData = {};
+
+    initJson(dir, initFile, configData, err => {
+      // TODO 错误捕获
+      err ? this.log(err) : this.log('package.json done!');// eslint-disable-line
     });
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
+  // 下载模版
+  // TODO 显示下载进度
+  _download() {
+    this.log(
+      'download template from https://github.com/58-magic/react-component-template'
+    );
+    DownloadGit(
+      'https://github.com:58-magic/react-component-template',
+      archiveDir,
+      err => {
+        // TODO 错误捕获
+        err ? this.log(err) : this.log('downloaded');// eslint-disable-line
+        this._handleArchive();// eslint-disable-line
+      }
     );
   }
 
-  install() {
-    this.installDependencies();
+  _getFilePath(filename) {
+    this.log(`handle ${filename}`);
+    return path.join(process.cwd(), archiveDir, filename);
+  }
+
+  _handleArchive() {
+    this.log('handleArchive');
+    this.log(this._getFilePath('.'));//eslint-disable-line
+    // 删除无用文件 封装
+    const deleteFileArr = ['LICENSE', 'package-lock.json', 'README.md'];
+    deleteFileArr.forEach(name => fsextra.removeSync(this._getFilePath(name)));// eslint-disable-line
+
+    // TODO 替换文件
+
+    // 复制，自动删除了archiveDir目录
+    fsextra.moveSync(this._getFilePath('.'), process.cwd());// eslint-disable-line
+
+    this.log('handle done!');
+
+    // merge package.json
+    this._writeJson();// eslint-disable-line
   }
 };
